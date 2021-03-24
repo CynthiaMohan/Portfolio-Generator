bbe30b7e0afcd04658f892496c9872ab0fcff325
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-03-24 15:13:24 -0700

    create and write to file index.html

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => {
-        console.log(profileItem);
-    });
+const [username, github] = profileDataArgs;
+console.log(username, github);
 
-};
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(username, github);
+
+
+
+fs.writeFile('index.html', generatePage(username, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Checkout index.html to see the output!')
+});
